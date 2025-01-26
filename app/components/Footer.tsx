75fdfc66e0a833575f3d27b635228c6355a1276d
Phil <philwekullo@gmail.com>
2025-01-26 12:19:54 +0300

    Footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <h1 className="text-lg font-marcellus text-center">&copy;Best Outcomes Youth Solace 2025</h1>
+    </footer>
+  )
+}
+
+export default Footer
